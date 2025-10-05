7e1613110c4c4b0b11eba3b383571fd611f4f482
Leandro Julcahuanca <ljulcahuanca17@gmail.com>
2025-10-05 16:06:46 -0500

    refactor: Añadir cambios mínimos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
     <div class="absolute w-full bg-red-50 dark:hidden min-h-72"></div>
     <x-app.sidebar class='z-20 max-w-64 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0'/>
     <main class="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-[17rem] rounded-xl z-10">
-        <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 ml-6 transition-all ease-in shadow-none xl:mr-1 lg:mr-10 duration-250 rounded-2xl lg:flex-nowrap lg:justify-start md:mr-10">
+        <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 ml-6 transition-all ease-in shadow-none xl:mr-2 lg:mr-10 duration-250 rounded-2xl lg:flex-nowrap lg:justify-start md:mr-10">
             <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
                 <nav>
                     <!-- breadcrumb -->
@@ -54,95 +54,110 @@
                                 placeholder="Type here..." />
                         </div>
                     </div>
-                    <ul class="flex flex-row justify-end pl-0 mb-0 -ml-1.5 list-none">
-                        <li class="flex items-center">
-                            <a href="#"
-                                class="relative flex flex-wrap px-4 py-2 text-sm font-semibold transition-all group hover:px-8 text-stone-950 hover:rounded-full hover:bg-red-100">
-                                <x-icon name='user' class="w-4 sm:hidden sm:mr-2 text-stone-950 group-hover:inline" />
-                                <span class="hidden pt-[1px] sm:inline">{{ __('Login') }}</span>
-                            </a>
-                        </li>
-                        <li class="relative flex items-center pr-2 ml-2">
-                            <x-dropdown align='right' width='72'>
-                                <x-slot name="trigger">
-                                    <a href="#" class="block p-0 text-sm transition-all text-stone-950 ease-nav-brand">
-                                        <x-icon name='bell' class="cursor-pointer" />
-                                    </a>
-                                </x-slot>
 
-                                <x-dropdown-link class='relative'>
-                                    <div class="flex py-1">
-                                        <div class="my-auto">
-                                            <img src="#"
-                                                class="inline-flex items-center justify-center mr-4 text-sm text-white h-9 w-9 max-w-none rounded-xl" />
-                                        </div>
-                                        <div class="flex flex-col justify-center">
-                                            <h6 class="mb-1 text-sm font-normal leading-normal dark:text-white">
-                                                <span class="font-semibold">New message</span> from Laur
-                                            </h6>
-                                            <p class="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
-                                                <i class="mr-1 fa fa-clock"></i>
-                                                13 minutes ago
-                                            </p>
-                                        </div>
-                                    </div>
-                                </x-dropdown-link>
+                    @guest
+                        <ul class="flex flex-row justify-end pl-0 mb-0 -ml-1.5 list-none">
+                            <li class="flex items-center">
+                                <a href="#"
+                                    class="relative flex flex-wrap px-4 py-2 text-sm font-semibold transition-all group hover:px-8 text-stone-950 hover:rounded-full hover:bg-red-100">
+                                    <x-icon name='user' class="w-4 sm:hidden sm:mr-2 text-stone-950 group-hover:inline" />
+                                    <span class="hidden pt-[1px] sm:inline">{{ __('Login') }}</span>
+                                </a>
+                            </li>
+                        </ul>
+                    @endguest
 
-                                <x-dropdown-link class="relative">
-                                    <div class="flex py-1">
-                                        <div class="my-auto">
-                                            <img src="#"
-                                                class="inline-flex items-center justify-center mr-4 text-sm text-white bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 h-9 w-9 max-w-none rounded-xl" />
-                                        </div>
-                                        <div class="flex flex-col justify-center">
-                                            <h6 class="mb-1 text-sm font-normal leading-normal dark:text-white">
-                                                <span class="font-semibold">New album</span> by Travis Scott
-                                            </h6>
-                                            <p class="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
-                                                <i class="mr-1 fa fa-clock"></i>
-                                                1 day
-                                            </p>
-                                        </div>
-                                    </div>
-                                </x-dropdown-link>
+                    @auth
+                        <ul class="flex flex-row justify-end pl-0 mb-0 -ml-1.5 list-none">
+                            <li class="flex items-center">
+                                <a href="{{ route('profile.show') }}"
+                                    class="relative flex flex-wrap px-4 py-2 text-sm font-semibold transition-all group hover:px-8 text-stone-950 hover:rounded-full hover:bg-red-100">
+                                    <x-icon name='user' class="w-4 sm:hidden sm:mr-2 text-stone-950 group-hover:inline" />
+                                    <span class="hidden pt-[1px] sm:inline">{{ __('Profile') }}</span>
+                                </a>
+                            </li>
+                            <li class="relative flex items-center pr-2 ml-2">
+                                <x-dropdown align='right' width='72'>
+                                    <x-slot name="trigger">
+                                        <a href="#" class="block p-0 text-sm transition-all text-stone-950 ease-nav-brand">
+                                            <x-icon name='bell' class="cursor-pointer" />
+                                        </a>
+                                    </x-slot>
 
-                                <x-dropdown-link class="relative">
-                                    <div class="flex py-1">
-                                        <div
-                                            class="inline-flex items-center justify-center my-auto mr-4 text-sm text-white transition-all duration-200 ease-nav-brand bg-gradient-to-tl from-slate-600 to-slate-300 h-9 w-9 rounded-xl">
-                                            <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1"
-                                                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                                <title>credit-card</title>
-                                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                                    <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF"
-                                                        fill-rule="nonzero">
-                                                        <g transform="translate(1716.000000, 291.000000)">
-                                                            <g transform="translate(453.000000, 454.000000)">
-                                                                <path class="color-background"
-                                                                    d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"
-                                                                    opacity="0.593633743"></path>
-                                                                <path class="color-background"
-                                                                    d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z">
-                                                                </path>
+                                    <x-dropdown-link class='relative'>
+                                        <div class="flex py-1">
+                                            <div class="my-auto">
+                                                <img src="#"
+                                                    class="inline-flex items-center justify-center mr-4 text-sm text-white h-9 w-9 max-w-none rounded-xl" />
+                                            </div>
+                                            <div class="flex flex-col justify-center">
+                                                <h6 class="mb-1 text-sm font-normal leading-normal dark:text-white">
+                                                    <span class="font-semibold">New message</span> from Laur
+                                                </h6>
+                                                <p class="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
+                                                    <i class="mr-1 fa fa-clock"></i>
+                                                    13 minutes ago
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </x-dropdown-link>
+
+                                    <x-dropdown-link class="relative">
+                                        <div class="flex py-1">
+                                            <div class="my-auto">
+                                                <img src="#"
+                                                    class="inline-flex items-center justify-center mr-4 text-sm text-white bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 h-9 w-9 max-w-none rounded-xl" />
+                                            </div>
+                                            <div class="flex flex-col justify-center">
+                                                <h6 class="mb-1 text-sm font-normal leading-normal dark:text-white">
+                                                    <span class="font-semibold">New album</span> by Travis Scott
+                                                </h6>
+                                                <p class="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
+                                                    <i class="mr-1 fa fa-clock"></i>
+                                                    1 day
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </x-dropdown-link>
+
+                                    <x-dropdown-link class="relative">
+                                        <div class="flex py-1">
+                                            <div
+                                                class="inline-flex items-center justify-center my-auto mr-4 text-sm text-white transition-all duration-200 ease-nav-brand bg-gradient-to-tl from-slate-600 to-slate-300 h-9 w-9 rounded-xl">
+                                                <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1"
+                                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                                    <title>credit-card</title>
+                                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                        <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF"
+                                                            fill-rule="nonzero">
+                                                            <g transform="translate(1716.000000, 291.000000)">
+                                                                <g transform="translate(453.000000, 454.000000)">
+                                                                    <path class="color-background"
+                                                                        d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"
+                                                                        opacity="0.593633743"></path>
+                                                                    <path class="color-background"
+                                                                        d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z">
+                                                                    </path>
+                                                                </g>
                                                             </g>
                                                         </g>
                                                     </g>
-                                                </g>
-                                            </svg>
+                                                </svg>
+                                            </div>
+                                            <div class="flex flex-col justify-center">
+                                                <h6 class="mb-1 text-sm font-normal leading-normal dark:text-white">
+                                                    Payment successfully completed</h6>
+                                                <p class="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
+                                                    <i class="mr-1 fa fa-clock"></i>
+                                                    2 days
+                                                </p>
+                                            </div>
                                         </div>
-                                        <div class="flex flex-col justify-center">
-                                            <h6 class="mb-1 text-sm font-normal leading-normal dark:text-white">
-                                                Payment successfully completed</h6>
-                                            <p class="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
-                                                <i class="mr-1 fa fa-clock"></i>
-                                                2 days
-                                            </p>
-                                        </div>
-                                    </div>
-                                </x-dropdown-link>
-                            </x-dropdown>
-                        </li>
-                    </ul>
+                                    </x-dropdown-link>
+                                </x-dropdown>
+                            </li>
+                        </ul>
+                    @endauth
                 </div>
             </div>
         </nav>
